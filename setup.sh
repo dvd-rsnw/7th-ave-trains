c629e0b384d83c4ab531d340f0df295283de5dc6
dvd-rsnw <david.duque@feathrapp.com>
2025-04-13 19:39:25 -0400

    fix: update script

--- setup.sh
+++ setup.sh
@@ -18,7 +18,7 @@ check_project_dir
 # Install system dependencies
 echo "Installing system dependencies..."
 sudo apt-get update
-sudo apt-get install -y python3-pip python3-venv git build-essential python3-dev python3-pillow
+sudo apt-get install -y python3-pip python3-venv python3-full git build-essential python3-dev python3-pillow
 
 # Clone and build rpi-rgb-led-matrix
 echo "Setting up RGB LED Matrix library..."
@@ -55,33 +55,32 @@ fi
 
 # Create new virtual environment
 echo "Creating new virtual environment..."
-sudo python3 -m venv .venv
-sudo chown -R $USER:$USER .venv
+python3 -m venv .venv
 
 # Configure virtual environment to show prompt
-sudo cat >> .venv/bin/activate << EOL
+cat >> .venv/bin/activate << EOL
 PS1="(.venv) \$PS1"
 EOL
 
 # Activate virtual environment and install dependencies
 echo "Installing Python dependencies..."
 source .venv/bin/activate
-sudo pip install --upgrade pip wheel setuptools
-sudo pip install -r requirements.txt
+.venv/bin/pip install --upgrade pip wheel setuptools
+.venv/bin/pip install -r requirements.txt
 
 # Install RGB Matrix Python module into virtual environment
 echo "Installing RGB Matrix Python module into virtual environment..."
 cd ~/rpi-rgb-led-matrix/bindings/python
 sudo rm -rf build  # Clean any existing build with wrong permissions
 sudo python3 setup.py clean --all
-sudo -H pip3 install .
+.venv/bin/pip install .
 
 # Return to project directory
 cd "$PROJECT_DIR"
 check_project_dir
 
 echo "Creating run script..."
-sudo cat > run.sh << EOF
+cat > run.sh << EOF
 #!/bin/bash
 
 # Check if running as root
@@ -116,8 +115,7 @@ kill \$API_PID
 EOF
 
 # Make run script executable
-sudo chmod +x run.sh
-sudo chown $USER:$USER run.sh
+chmod +x run.sh
 
 echo "Setup complete! To run the display:"
 echo "  cd $PROJECT_DIR"
